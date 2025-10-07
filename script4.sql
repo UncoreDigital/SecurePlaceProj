5839655ec6f4297e6e5fc4c8d393d91163e35467
neha <neha75031@gmail.com>
2025-10-07 22:42:58 +0530

    script issue fix

--- script4.sql
+++ script4.sql
@@ -2,6 +2,8 @@
 INSERT INTO firms (name) VALUES ('Default Firm')
 ON CONFLICT DO NOTHING;
 
+INSERT INTO "auth"."users" ("instance_id", "id", "aud", "role", "email", "encrypted_password", "email_confirmed_at", "invited_at", "confirmation_token", "confirmation_sent_at", "recovery_token", "recovery_sent_at", "email_change_token_new", "email_change", "email_change_sent_at", "last_sign_in_at", "raw_app_meta_data", "raw_user_meta_data", "is_super_admin", "created_at", "updated_at", "phone", "phone_confirmed_at", "phone_change", "phone_change_token", "phone_change_sent_at",  "email_change_token_current", "email_change_confirm_status", "banned_until", "reauthentication_token",  "is_sso_user", "deleted_at", "is_anonymous") VALUES ('00000000-0000-0000-0000-000000000000', '2853d098-9274-4525-a16d-f0d1bbe49298', 'authenticated', 'authenticated', 'neha@gmail.com', '$2a$10$KiixYNjYAaH68Ezob18Ab.xIeAwLnyaLhoylIkquJj5G3f6Wq7/J2', '2025-09-18 06:55:13.658731+00', null, '', null, '', null, '', '', null, '2025-10-06 19:35:23.852219+00', '{"provider": "email", "providers": ["email"]}', '{"email_verified": true}', null, '2025-09-18 06:55:13.654493+00', '2025-10-07 13:54:30.846546+00', null, null, '', '', null,  '', '0', null, '', 'false', null, 'false');
+
 -- Get firm id
 WITH f AS (
   SELECT id FROM firms WHERE name = 'Default Firm' LIMIT 1
--- script5.sql
+++ script5.sql
@@ -1,4 +1,33 @@
 -- Update by email (replace with your actual email)
+-- INSERT INTO auth.users (id, email)
+-- VALUES ('2853d098-9274-4525-a16d-f0d1bbe49297', 'neha.uncoredigital@gmail.com');  
+
+
+-- UPDATE auth.users
+-- SET encrypted_password = '$2a$10$KiixYNjYAaH68Ezob18Ab.xIeAwLnyaLhoylIkquJj5G3f6Wq7/J2'
+-- WHERE id = '2853d098-9274-4525-a16d-f0d1bbe49297';
+
+-- Then create the user profile
+-- INSERT INTO user_profiles (
+--   id,
+--   email,
+--   first_name,
+--   last_name,
+--   role,
+--   is_active,
+--   last_login_at
+-- )
+-- VALUES (
+--   '2853d098-9274-4525-a16d-f0d1bbe49297',
+--   'neha.uncoredigital@gmail.com',
+--   'Admin',
+--   'User',
+--   'super_admin',
+--   true,
+--   NOW()
+-- );
+
+
 UPDATE user_profiles 
 SET role = 'super_admin' 
 WHERE email = 'neha.uncoredigital@gmail.com';
